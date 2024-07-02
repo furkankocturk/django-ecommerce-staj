970181be40f56cbb5bb9462b7a71aae657f3e273
Dilara Okşaksin <dilaraoksaksin@gmail.com>
2024-07-02 17:41:19 +0300

    kategoriler ve markalar eklendi, footer eklendi, menü yeniden düzenlendi

--- shop/urls.py
+++ shop/urls.py
@@ -4,3 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
 ]
+urlpatterns = [
+    path("", views.home, name='home'),
+    path("", views.home, name='link'),
+    path("", views.home, name='Action'),
+    path("", views.home, name='Another action'),
+    path("", views.home, name='something else here'),
+path("", views.home, name='disable'),
+path("", views.home, name='favorilerim'),
+path("", views.home, name='sepetim'),
+]
